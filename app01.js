713f4d1e9c27b348fa21f7507b9bb5e89520ef9d
MarianaAspilcuetaClaudio <a20150023@juan23.edu.pe>
2024-10-27 15:24:47 -0500

    Add files via upload

---
+++ app01.js
@@ -0,0 +1,5 @@
+document.write("<h1>Bienvendio a la pagina de javascript</h1>");
+var nom = prompt("ingrese su nombre");
+document.write(
+  "hola " + nom + " es tu primer contacto con la programacion javascript"
+);
